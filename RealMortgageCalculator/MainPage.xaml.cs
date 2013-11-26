56fc48295cca1b8b6427c79aace1aeb6c6c61bbc
jmherruzo <josemaherruzo@hotmail.com>
2013-11-26 16:17:51 +0100

    Action bar

--- RealMortgageCalculator/MainPage.xaml.cs
+++ RealMortgageCalculator/MainPage.xaml.cs
@@ -23,19 +23,19 @@ namespace RealMortgageCalculator
         }
 
         // Código de ejemplo para compilar una ApplicationBar traducida
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Establecer ApplicationBar de la página en una nueva instancia de ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
+        private void BuildLocalizedApplicationBar()
+        {
+            // Establecer ApplicationBar de la página en una nueva instancia de ApplicationBar.
+            ApplicationBar = new ApplicationBar();
 
-        //    // Crear un nuevo botón y establecer el valor de texto en la cadena traducida de AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
+            // Crear un nuevo botón y establecer el valor de texto en la cadena traducida de AppResources.
+            ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+            appBarButton.Text = AppResources.AppBarButtonText;
+           ApplicationBar.Buttons.Add(appBarButton);
 
-        //    // Crear un nuevo elemento de menú con la cadena traducida de AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+            // Crear un nuevo elemento de menú con la cadena traducida de AppResources.
+            ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+            ApplicationBar.MenuItems.Add(appBarMenuItem);
+        }
     }
 }
