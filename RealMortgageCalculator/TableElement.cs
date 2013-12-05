b4876661ef437c020a7cdca322686b0b835cf179
jmherruzo <josemaherruzo@hotmail.com>
2013-12-05 10:46:23 +0100

    Añadir Elemento de tabla

---
+++ RealMortgageCalculator/TableElement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealMortgageCalculator
+{
+
+    /**
+     * Clase usada para representar cada elemento de la tabla de amortización
+     **/
+    class TableElement
+    {
+        public String mes { get; set; }
+        public String deuda { get; set; }
+        public String amortizacion { get; set; }
+        public String interes { get; set; }
+    }
+}
